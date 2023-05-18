2f4051edbf8ace250091bbc57fc672b019c429a6
Rodrigo Morais <58960796+rodigu@users.noreply.github.com>
2023-05-18 16:10:36 -0500

    walker example additions

--- sketch/examples/walkingCycle/walker.ts
+++ sketch/examples/walkingCycle/walker.ts
@@ -1,3 +1,4 @@
+import { BaseBehaviors } from "../../lib/baseBehaviors";
 import { Entity } from "../../lib/entity";
 import { GameManager } from "../../lib/manager";
 import { SpriteAnimation } from "../../lib/spriteAnimation";
@@ -11,5 +12,35 @@ function createWalker(manager: GameManager) {
     4
   );
 
-  const walker = new Entity(manager.p, "walker");
+  const walker = new Entity(
+    manager.p,
+    "walker",
+    0,
+    { width: 50, height: 50 },
+    { x: 0, y: 0 },
+    0
+  );
+
+  const spriteAnimationFunctions = BaseBehaviors.addSpriteAnimation(
+    walker,
+    walkingCycleTileset
+  );
+
+  walker.activateBehavior(BaseBehaviors.Names.SpriteAnimation);
+
+  spriteAnimationFunctions.setCurrentSpriteFunction("walking");
+  spriteAnimationFunctions.newCycleFunction({
+    cycleName: "walking",
+    frames: [3, 4, 5],
+    timing: 60,
+  });
+
+  manager.addEntity("walker", walker, walker.layer);
+
+  manager.addState("draw-entities", (m: GameManager) => {
+    m.p.push();
+    m.p.translate(manager.p.width / 2, manager.p.height / 2);
+    m.runEntities();
+    m.p.pop();
+  });
 }
